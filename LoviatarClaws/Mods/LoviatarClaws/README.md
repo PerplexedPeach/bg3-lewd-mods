80dead9eef48f62324860bdcceb2492e1bd36e1c
LazyIcarus <kleinmorett@gmail.com>
2023-09-10 11:42:02 -0400

    Add debuff when your body is remodelled and you are not wearing the claws

--- LoviatarClaws/Mods/LoviatarClaws/README.md
+++ LoviatarClaws/Mods/LoviatarClaws/README.md
@@ -1,12 +1,30 @@
-## Scripted Effects
+## Features
+- tintable
+- thematic passive and active abilities
+- optional body modification content
+
+## How to Get
+- tutorial chest that you can also get via [summoning](https://www.nexusmods.com/baldursgate3/mods/457)
+- `07ea67e5-344b-4de6-91d3-449cce27a685` spawn with script
+- Abdirak (Loviatar priest in the goblin camp) inventory
+
+## Body Modification (requires bg3se)
 If you have Loviatar's Love (status boost that lasts until you die), taking a long rest with the boots on will progressively advance `Remodelled Frame` permanent boost, up to 4 times, with effects listed below. The theme is muscle loss, tendon shortening, and so on (no model change yet).
 To avoid body modification, die to remove Loviatar's Love.
 ```
--1 STR
--1 STR -1 DEX +1 CHA
--2 STR -1 DEX +1 CHA disadvantage on intimidation checks
--2 STR +2 CHA advantage on Persuasion checks, disadvantage on intimidation checks
+Inception: -1 STR
+Foundation: -1 STR -1 DEX +1 CHA
+Maturation: -2 STR -1 DEX +1 CHA disadvantage on intimidation checks
+Perfection: -2 STR +2 CHA advantage on Persuasion checks, disadvantage on intimidation checks
 ```
 
-Additionally, with stacks of `Remodelled Frame`, removing the boots will give debuff until you re-equip the boots, the severity of which stacks with the `Remodelled Frame` stacks.
-With each stack, you receive `-1m movement speed, -1 DEX`.
+Additionally, with stacks of `Remodelled Frame`, removing the boots will give a debuff until you re-equip the boots, the severity of which stacks with the `Remodelled Frame` stacks.
+With each stack, you receive `-1m movement speed, -1 DEX`. At 4 stacks, you also receive disadvantage on DEX saving throws. This is due to your feet having been remodelled to need the heel support to walk, as otherwise you're either crawling on your knees or walking on tip-toes.
+
+## Installation
+- unzip and copy the `.pak` file to `C:\Users\<USER>\AppData\Local\Larian Studios\Baldur's Gate 3\Mods` as per usual
+- activate via the [bg3 mod manager](https://github.com/LaughingLeader/BG3ModManager/releases) as per usual
+- install [BG3SE (script extender)](https://github.com/Norbyte/bg3se/releases)
+
+## Credits
+[weslan](https://www.nexusmods.com/baldursgate3/users/89809) for the initial port of [Ciri's clothing](https://www.nexusmods.com/baldursgate3/mods/519) that I used as the base model.
--- LoviatarClaws/Mods/LoviatarClaws/ScriptExtender/Lua/Server/CursedEffects.lua
+++ LoviatarClaws/Mods/LoviatarClaws/ScriptExtender/Lua/Server/CursedEffects.lua
@@ -12,9 +12,10 @@
 
 -- Ext.Events.SessionLoaded:Subscribe("SessionLoaded", OnSessionLoaded)
 
-local WEARER_KEY = "LoviatarClawsWearer";
 local loviatar_claws_item_id = "LI_LoviatarClaws_07ea67e5-344b-4de6-91d3-449cce27a685";
 local remodelled_frame_id_prefix = "LI_Claws_RemodelledFrame_";
+local remodelled_frame_removed_group = "LI_Claws_RemodelledFrame_removed";
+local remodelled_frame_removed_id_prefix = remodelled_frame_removed_group .. "_";
 local remodelled_message = {
     "You wake up, feeling that your body has subtly changed. Your muscles feels softer and your flesh feels fuller in certain places.",
     "Your body feels more sensitive to touch. You feel a strange urge to join the frontline of the fiercest battle you can find.",
@@ -22,13 +23,17 @@ local remodelled_message = {
     "Again, you wake up to a softer body. Your full curves and supple limbs seem more appropriate for a bordella than a life on the road. You have now grown used to the grace of your new body."
 };
 
+local function _I(msg)
+    _P("[LoviatarClaws] " .. msg);
+end
+
 local function getCurrentWearer()
     for _, player in pairs(Osi["DB_Players"]:Get(nil)) do
         local char = player[1];
         local boots = Osi.GetEquippedItem(char, "Boots");
-        _P("Character " .. char .. " has boots " .. tostring(boots));
+        _I("Character " .. char .. " has boots " .. tostring(boots));
         if boots ~= nil and Osi.GetTemplate(boots) == loviatar_claws_item_id then
-            _P("Found wearer: " .. char);
+            _I("Found wearer: " .. char);
             return char;
         end
     end
@@ -39,18 +44,20 @@ local function hasLoviatarsLove(char)
     return Osi.HasActiveStatus(char, "GOB_CALMNESS_IN_PAIN");
 end
 
-local function upgradeRemodelledFrame(char)
-    -- check level of remodelled body
-    _P("Checking remodelled body level of " .. char);
-    local current_level = 0;
+local function remodelledFrameLevel(char)
     for level = 1, 4 do
         local passive_key = remodelled_frame_id_prefix .. level;
         if Osi.HasPassive(char, passive_key) == 1 then
-            current_level = level;
-            Osi.RemovePassive(char, passive_key);
+            return level;
         end
     end
-    _P("Upgrading remodelled body to level " .. current_level + 1);
+    return 0;
+end
+
+local function upgradeRemodelledFrame(char)
+    -- check level of remodelled body
+    local current_level = remodelledFrameLevel(char);
+    _I("Upgrading remodelled body to level " .. current_level + 1);
     Osi.AddPassive(char, remodelled_frame_id_prefix .. current_level + 1);
     Osi.OpenMessageBox(char, remodelled_message[current_level + 1]);
 end
@@ -59,27 +66,39 @@ local function curseLongRestHandler()
     -- loop over characters and check if anyway was wearing the cursed item
     local wearer = getCurrentWearer();
     if wearer ~= nil then
-        _P("Loviatar Claws wearer found: " .. wearer);
+        _I("Loviatar Claws wearer found: " .. wearer);
         -- check that the wearer has Loviatar's blessing
         -- if not, then they can no longer proceed with the transformation
         if not hasLoviatarsLove(wearer) then
             return
         end
 
-        -- no need to add persistent checks since only one character can have Loviatar's Love
-        -- only this character can receive the effects from wearing it
-        -- if PersistentVars[WEARER_KEY] == nil then
-        --     PersistentVars[WEARER_KEY] = wearer;
-        -- end
-
         upgradeRemodelledFrame(wearer);
     end
 end
 
-Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", function()
-    _P("Long rest finished");
-    curseLongRestHandler();
-    --   if quest == "LOW_FindBhaalTemple_FollowMurders" then
-    --       Osi.TemplateAddTo("443b2caf-8d36-42cf-b389-d774229ed18c", GetHostCharacter(), 1, 1)
-    --   end
-end)
+local function curseItemUnequipHandler(template, char)
+    if template == loviatar_claws_item_id then
+        local level = remodelledFrameLevel(char);
+        _I("Loviatar Claws wearer unequipped item with level " .. level .. " remodelled body");
+        if level > 0 then
+            Osi.ApplyStatus(char, remodelled_frame_removed_id_prefix .. level, -1);
+        end
+    end
+end
+local function curseItemReequipHandler(template, char)
+    if template == loviatar_claws_item_id then
+        -- safe to use this level since character cannot progress to next level without wearing the claws
+        local level = remodelledFrameLevel(char);
+        if level > 0 then
+            _I("Loviatar Claws wearer reequipped claws");
+            Osi.RemoveStatus(char, remodelled_frame_removed_id_prefix .. level)
+        end
+    end
+end
+
+Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", curseLongRestHandler);
+
+-- handle curse from item unequip
+Ext.Osiris.RegisterListener("TemplateUnequipped", 2, "after", curseItemUnequipHandler);
+Ext.Osiris.RegisterListener("TemplateEquipped", 2, "after", curseItemReequipHandler);
