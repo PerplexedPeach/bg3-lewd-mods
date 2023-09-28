b6a439921937bce644e637cc6bb9efe717ee41c1
LazyIcarus <kleinmorett@gmail.com>
2023-09-27 21:27:23 -0400

    Add scripts and register handlers

---
+++ LoviatarClawsAlt/Mods/LoviatarClawsAlt/ScriptExtender/Lua/Server/CursedEffects.lua
@@ -0,0 +1,7 @@
+Claws = Mods.LoviatarClaws.LiClawsProgression:new("ClawsAlt",
+    "LI_LoviatarClaws_Alt_75d51dc4-cc69-4b8a-8d92-61a841682866",
+    "LI_LoviatarClaws_Camp_Alt_1414dba3-2720-47b2-96f0-685bb8b2f319",
+    "LI_LoviatarClaws_Replica_Alt_c8d403fc-077a-43b6-a48e-0e622696745b");
+Claws:registerHandlers();
+-- Mods.LoviatarClaws.PersistentVars["Total Damage Taken"] = 100
+
