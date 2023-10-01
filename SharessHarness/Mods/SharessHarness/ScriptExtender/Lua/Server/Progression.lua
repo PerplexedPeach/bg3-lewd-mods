574cdb5da64ce86c0b313b70f00cd0d05281db23
LazyIcarus <kleinmorett@gmail.com>
2023-10-01 16:14:03 -0400

    Make script a framework to allow alternate forms of armor

--- SharessHarness/Mods/SharessHarness/ScriptExtender/Lua/Server/Progression.lua
+++ SharessHarness/Mods/SharessHarness/ScriptExtender/Lua/Server/Progression.lua
@@ -1,26 +1,6 @@
--- PersistentVars = {}
-
--- -- Variable will be restored after the savegame finished loading
--- local function doStuff()
---     PersistentVars['Test'] = 'Something to keep'
--- end
-
--- local function OnSessionLoaded()
---     -- Persistent variables are only available after SessionLoaded is triggered!
---     Ext.Print(PersistentVars['Test'])
--- end
-
--- Ext.Events.SessionLoaded:Subscribe("SessionLoaded", OnSessionLoaded)
-
 local remodelled_frame_id_prefix = "LI_Claws_RemodelledFrame_";
 
-local harness_template_ids = {
-    "LI_SharessHarness_f84dabb1-f1da-467a-9236-8c6aa474d4a4",
-    "LI_SharessHarness_1_62328163-7cb9-49b5-8e76-f4a4ebd345ec",
-    "LI_SharessHarness_2_e54bf9e5-38d7-402f-8e6a-2f3d9e6baf9b",
-    "LI_SharessHarness_3_72dcd9e5-206b-48ba-83cc-997dc0598a57",
-    "LI_SharessHarness_4_2de908cd-285d-4a31-b259-cdec0d3b59c0"
-};
+
 local sated_status_ids = {
     "LI_SATED",
     "LI_SATED_1",
@@ -33,42 +13,6 @@ local function _I(msg)
     _P("[SharessHarness] " .. msg);
 end
 
-function HarnessLevel(char)
-    local worn = Osi.GetEquippedItem(char, "Breast");
-    _I("Character " .. char .. " wearing breasts " .. tostring(worn));
-    if worn == nil then
-        return nil;
-    end
-    for level = 1, 5 do
-        local harness_template_id = harness_template_ids[level];
-        if Osi.GetTemplate(worn) == harness_template_id then
-            _I("Found armor stage: " .. level - 1);
-            return level - 1;
-        end
-    end
-    return nil;
-end
-
-local function getSatedWearer()
-    -- return the character and the number of sated stacks
-    for _, player in pairs(Osi["DB_Players"]:Get(nil)) do
-        local char = player[1];
-        for stacks, status_id in pairs(sated_status_ids) do
-            if Osi.HasActiveStatus(char, status_id) == 1 then
-                _I("Found sated wearer: " .. char .. " with stacks " .. stacks);
-                return char, stacks
-            end
-        end
-        -- check if this character is wearing it but has no stacks
-        local harness_level = HarnessLevel(char);
-        if harness_level ~= nil then
-            return char, 0
-        end
-    end
-    _I("No sated wearer found");
-    return nil, 0
-end
-
 
 function RemodelledFrameLevel(char)
     for level = 1, 4 do
@@ -78,6 +22,64 @@ function RemodelledFrameLevel(char)
         end
     end
     return 0;
+end
+
+---@class LiHarnessProgression
+LiHarnessProgression = {};
+---Main progression interface for Sharess's harness
+---@param main_shortname string Short name for logging
+---@param item_ids table list of full template ID for the armor item e.g. LI_LoviatarClaws_07ea67e5-344b-4de6-91d3-449cce27a685
+---@return LiHarnessProgression
+function LiHarnessProgression:new(main_shortname, item_ids)
+    local self = {} ---@class LiHarnessProgression
+    setmetatable(self, {
+        __index = LiHarnessProgression
+    })
+
+    self.shortname = main_shortname;
+    self.item_ids = item_ids;
+
+    return self;
+end
+
+function LiHarnessProgression:log(message)
+    _P("[" .. self.shortname .. "] " .. message);
+end
+
+function LiHarnessProgression:harnessLevel(char)
+    local worn = Osi.GetEquippedItem(char, "Breast");
+    self:log("Character " .. char .. " wearing breasts " .. tostring(worn));
+    if worn == nil then
+        return nil;
+    end
+    for level = 1, 5 do
+        local harness_template_id = self.item_ids[level];
+        if Osi.GetTemplate(worn) == harness_template_id then
+            self:log("Found armor stage: " .. level - 1);
+            return level - 1;
+        end
+    end
+    return nil;
+end
+
+function LiHarnessProgression:getSatedWearer()
+    -- return the character and the number of sated stacks
+    for _, player in pairs(Osi["DB_Players"]:Get(nil)) do
+        local char = player[1];
+        for stacks, status_id in pairs(sated_status_ids) do
+            if Osi.HasActiveStatus(char, status_id) == 1 then
+                self:log("Found sated wearer: " .. char .. " with stacks " .. stacks);
+                return char, stacks
+            end
+        end
+        -- check if this character is wearing it but has no stacks
+        local harness_level = self:harnessLevel(char);
+        if harness_level ~= nil then
+            return char, 0
+        end
+    end
+    self:log("No sated wearer found");
+    return nil, 0
 end
 
 local function delayedCall(delayInMs, func)
@@ -92,27 +94,27 @@ local function delayedCall(delayInMs, func)
     end);
 end
 
-local function upgradeArmor(char)
+function LiHarnessProgression:upgradeArmor(char)
     -- check level of remodelled body
     local body_level = RemodelledFrameLevel(char);
     -- can only upgrade to the remodelled body stage
-    local armor_level = HarnessLevel(char);
+    local armor_level = self:harnessLevel(char);
     -- not wearing the harness
     -- TODO give a message to the player that they need to be wearing the harness
     if armor_level == nil then
-        _I("Character " .. char .. " is not wearing the harness");
+        self:log("Character " .. char .. " is not wearing the harness");
         -- Osi.OpenMessageBox(char, acquire_message);
         return
     end
 
     if body_level > armor_level then
-        _I("Body level " .. body_level .. " is higher than armor level " .. armor_level .. ", upgrading armor");
+        self:log("Body level " .. body_level .. " is higher than armor level " .. armor_level .. ", upgrading armor");
         -- remove the old armor
-        local old_armor_template = harness_template_ids[armor_level + 1];
+        local old_armor_template = self.item_ids[armor_level + 1];
         Osi.TemplateRemoveFromUser(old_armor_template, char, 1);
 
         -- add the new armor
-        local new_armor_template = harness_template_ids[armor_level + 2];
+        local new_armor_template = self.item_ids[armor_level + 2];
         Osi.TemplateAddTo(new_armor_template, char, 1, 1);
         -- if new_armor == nil then
         --     _I("Failed to find new armor " .. new_armor_template .. " in inventory");
@@ -122,26 +124,26 @@ local function upgradeArmor(char)
         -- this is because the armor is not available to equip immediately after adding it
         delayedCall(1000, function()
             local new_armor = Osi.GetItemByTemplateInUserInventory(new_armor_template, char);
-            _I("Equipping new armor " .. tostring(new_armor));
+            self:log("Equipping new armor " .. tostring(new_armor));
             Osi.Equip(char, new_armor);
         end);
     end
 end
 
-local function curseLongRestHandler()
+function LiHarnessProgression:curseLongRestHandler()
     -- loop over characters and check if anyone has 3 sated stacks
-    local wearer, stacks = getSatedWearer();
+    local wearer, stacks = self:getSatedWearer();
     if wearer ~= nil then
-        _I("Sharess Harness wearer found: " .. wearer .. " with " .. stacks .. " stacks");
+        self:log("Sharess Harness wearer found: " .. wearer .. " with " .. stacks .. " stacks");
 
         -- check that the armor can still be upgraded
-        local armor_level = HarnessLevel(wearer);
+        local armor_level = self:harnessLevel(wearer);
         -- armor level is from 0 to 4, so if it's 4 then we can't upgrade it anymore
         if armor_level ~= nil and armor_level < 4 then
             -- if we have fewer than 3 stacks, we don't need to do anything
             -- give a message to the player hinting that they need to get more stacks
             if stacks >= 2 then
-                upgradeArmor(wearer);
+                self:upgradeArmor(wearer);
             elseif stacks >= 0 then
                 Osi.OpenMessageBox(wearer, need_more_sated_stacks_msg);
             end
@@ -154,4 +156,16 @@ local function curseLongRestHandler()
     end
 end
 
-Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", curseLongRestHandler);
+function LiHarnessProgression:registerHandlers()
+    Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", function() self:curseLongRestHandler() end);
+    self:log("Registered handlers");
+end
+
+Harness = LiHarnessProgression:new("Harness", {
+    "LI_SharessHarness_f84dabb1-f1da-467a-9236-8c6aa474d4a4",
+    "LI_SharessHarness_1_62328163-7cb9-49b5-8e76-f4a4ebd345ec",
+    "LI_SharessHarness_2_e54bf9e5-38d7-402f-8e6a-2f3d9e6baf9b",
+    "LI_SharessHarness_3_72dcd9e5-206b-48ba-83cc-997dc0598a57",
+    "LI_SharessHarness_4_2de908cd-285d-4a31-b259-cdec0d3b59c0"
+});
+Harness:registerHandlers();
