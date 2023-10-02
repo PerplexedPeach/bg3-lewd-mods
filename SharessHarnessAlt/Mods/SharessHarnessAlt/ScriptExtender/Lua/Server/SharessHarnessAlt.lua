48839094d9a7c140173aeae55058b64ce58ab5a7
LazyIcarus <kleinmorett@gmail.com>
2023-10-02 00:00:44 -0400

    Initialize Sharess robe alt

---
+++ SharessHarnessAlt/Mods/SharessHarnessAlt/ScriptExtender/Lua/Server/SharessHarnessAlt.lua
@@ -0,0 +1,8 @@
+Robe = Mods.SharessHarness.LiHarnessProgression:new("Sharess Robe", {
+    "LI_SharessHarness_Robe_4cca4166-4662-43c9-8d54-ed40c098117b",
+    "LI_SharessHarness_Robe_1_c1099b28-a68d-45d3-9b2e-a0ecadd82a13",
+    "LI_SharessHarness_Robe_2_d68447a5-8118-488e-8889-f2df5e00e6c1",
+    "LI_SharessHarness_Robe_3_d495219d-6f26-4ff9-8782-075236fc51c6",
+    "LI_SharessHarness_Robe_4_19ed1429-5761-405a-80b6-4d938393b8af"
+});
+Robe:registerHandlers();
