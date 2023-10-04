8839997d2dabe0bff9cf2810ac5117651837b643
LazyIcarus <kleinmorett@gmail.com>
2023-10-03 23:49:34 -0400

    Add stiletto heels alternate to claws (only F model)

---
+++ LoviatarClawsHeels/Mods/LoviatarClawsHeels/ScriptExtender/Lua/BoostrapServer.lua
@@ -0,0 +1,1 @@
+Ext.Require("Server/LoviatarClawsHeels.lua")
---
+++ LoviatarClawsHeels/Mods/LoviatarClawsHeels/ScriptExtender/Lua/Server/LoviatarClawsHeels.lua
@@ -0,0 +1,5 @@
+Heels = Mods.LoviatarClaws.LiClawsProgression:new("Claws Heels",
+    "LI_LoviatarClaws_Heels_b03e5646-ba7f-493c-a3f9-b642f4c5cdf8",
+    "LI_LoviatarClaws_Heels_Camp_8c0f0080-bb79-4b43-a788-bc033645fa70",
+    "LI_LoviatarClaws_Heels_Replica_041da15c-1906-41ef-bb27-ee7a05c02c47");
+Heels:registerHandlers(); 
