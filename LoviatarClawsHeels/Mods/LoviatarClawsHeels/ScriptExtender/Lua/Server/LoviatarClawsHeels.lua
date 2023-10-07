bb416492de7058c28e23e811af45ef1225cc82e1
LazyIcarus <kleinmorett@gmail.com>
2023-10-07 01:58:16 -0400

    Add open toe heels variant

--- LoviatarClawsHeels/Mods/LoviatarClawsHeels/ScriptExtender/Lua/Server/LoviatarClawsHeels.lua
+++ LoviatarClawsHeels/Mods/LoviatarClawsHeels/ScriptExtender/Lua/Server/LoviatarClawsHeels.lua
@@ -3,3 +3,9 @@ Heels = Mods.LoviatarClaws.LiClawsProgression:new("Claws Heels",
     "LI_LoviatarClaws_Heels_Camp_8c0f0080-bb79-4b43-a788-bc033645fa70",
     "LI_LoviatarClaws_Heels_Replica_041da15c-1906-41ef-bb27-ee7a05c02c47");
 Heels:registerHandlers(); 
+
+HeelsOpen = Mods.LoviatarClaws.LiClawsProgression:new("Claws HeelsOpen",
+    "LI_LoviatarClaws_HeelsOpen_84dcbcd4-2612-4030-aa16-d839e4904d74",
+    "LI_LoviatarClaws_HeelsOpen_Camp_1388f279-022e-4184-9ddc-cee6f306984c",
+    "LI_LoviatarClaws_HeelsOpen_Replica_1bd111a4-0f97-4e55-9219-d1a6b254d3d0");
+HeelsOpen:registerHandlers(); 
