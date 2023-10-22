351ec4fe613f7547411262b28e873563e0592050
LazyIcarus <kleinmorett@gmail.com>
2023-10-22 17:21:57 -0400

    Create leotard (doesn't work)

---
+++ SharessHarnessLeotard/Mods/SharessHarnessLeotard/ScriptExtender/Lua/Server/SharessHarnessLeotard.lua
@@ -0,0 +1,18 @@
+Leotard = Mods.SharessHarness.LiHarnessProgression:new("Sharess Leo", {
+    "LI_SharessHarnessLeotard_076a69a4-0393-4298-8aa9-7f3cbe9f0911",
+    "LI_SharessHarnessLeotard_1_18ad07e8-49a3-4a7d-b663-b710785c5019",
+    "LI_SharessHarnessLeotard_2_a22e815a-816d-4be0-9c95-318d369ed968",
+    "LI_SharessHarnessLeotard_3_68544a71-65b4-43fa-8f9d-c26cd051736c",
+    "LI_SharessHarnessLeotard_4_4fe14e6e-4499-4821-8248-aa7cd96fb312"
+});
+Leotard:registerHandlers();
+
+LeotardBare = Mods.SharessHarness.LiHarnessProgression:new("Sharess LeoBare", {
+    "LI_SharessHarnessLeotardBare_10fb656d-5e68-46b2-895a-85f03e6540df",
+    "LI_SharessHarnessLeotardBare_1_1b9b0546-8205-4276-a3c6-3f101c6ec2e4",
+    "LI_SharessHarnessLeotardBare_2_6f9d56cf-73b9-4f0d-915d-193bac57b9a4",
+    "LI_SharessHarnessLeotardBare_3_d26b4fb5-4737-41d8-abf1-828eafd7eaca",
+    "LI_SharessHarnessLeotardBare_4_892bca54-5c0e-4909-b7e5-5437a992f37a"
+});
+LeotardBare:registerHandlers();
+ 
