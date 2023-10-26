524de573a83068e0883bb555f789c8cbaa4874dc
LazyIcarus <kleinmorett@gmail.com>
2023-10-26 19:33:08 -0400

    Automatically upgrade fishnets

---
+++ UmberleeNets/Mods/UmberleeNets/ScriptExtender/Lua/Server/UmberleeNets.lua
@@ -0,0 +1,27 @@
+Nets = Mods.DivineCurse.BodyEquipment:new("Nets", "Underwear", {
+    "LI_UmberleeNets_372c66a5-8211-4ff1-86b2-f10bb1a84167",
+    "LI_UmberleeNets_1_0dbf9c7d-930e-484d-8ac0-632490b00c39",
+    "LI_UmberleeNets_2_0818e267-9727-49dc-bc3a-acf3dfaf185a",
+    "LI_UmberleeNets_3_b21ed2ae-9735-4b94-aef1-5e593935e11c",
+    "LI_UmberleeNets_4_7400a182-e2a9-43ce-af0b-52222a475192"
+});
+Nets:registerHandlers();
+
+
+NetsUnderwear = Mods.DivineCurse.BodyEquipment:new("NetsUnderwear", "Underwear", {
+    "LI_UmberleeNets_Suc_6a0291b8-f8a9-4659-a46d-9f2f9231ca4e",
+    "LI_UmberleeNets_Suc_1_72daccbf-ad39-416f-a907-539feaa7ca4d",
+    "LI_UmberleeNets_Suc_2_853c6971-7257-4e7d-85b4-00315e9ea0da",
+    "LI_UmberleeNets_Suc_3_7dd8e863-e810-4436-8956-c2082c115445",
+    "LI_UmberleeNets_Suc_4_ae79b53a-776b-47d4-849d-e68f42a76c89"
+});
+NetsUnderwear:registerHandlers();
+
+NetsSame = Mods.DivineCurse.BodyEquipment:new("NetsSame", "Underwear", {
+    "LI_UmberleeNets_Same_fa0cbc86-9691-406d-8cb3-5f62110ce035",
+    "LI_UmberleeNets_Same_1_e0ea3e12-07a2-4643-83f4-107b1ef36e23",
+    "LI_UmberleeNets_Same_2_6e0b66ac-0692-4550-9b0c-708e4511cf7a",
+    "LI_UmberleeNets_Same_3_3a06cebc-25a2-43c0-8e49-14e19d93f562",
+    "LI_UmberleeNets_Same_4_5904578f-4aa4-47ae-a510-b5907ee5853f"
+});
+NetsSame:registerHandlers();
