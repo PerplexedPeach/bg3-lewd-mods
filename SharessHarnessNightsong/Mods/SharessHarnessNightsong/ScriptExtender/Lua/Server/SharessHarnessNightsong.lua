e097e0907e3c277b81e79c0baddac7cd2ea08688
LazyIcarus <kleinmorett@gmail.com>
2023-10-29 18:22:15 -0400

    Register script

--- SharessHarnessNightsong/Mods/SharessHarnessNightsong/ScriptExtender/Lua/Server/SharessHarnessNightsong.lua
+++ SharessHarnessNightsong/Mods/SharessHarnessNightsong/ScriptExtender/Lua/Server/SharessHarnessNightsong.lua
@@ -1,1 +1,8 @@
- 
+_Nightsong = Mods.SharessHarness.LiHarnessProgression:new("Sharess Nightsong", {
+    "LI_SharessHarness_Nightsong_d5bc38a8-a90c-4f4f-bb00-2fba9a4f26eb",
+    "LI_SharessHarness_Nightsong_1_aebd35f9-e2c5-42f6-ab8b-4eb4c523a6d6",
+    "LI_SharessHarness_Nightsong_2_f7fa76de-4c1c-410e-bed0-3ec862abd51f",
+    "LI_SharessHarness_Nightsong_3_d822c7e3-9526-40ca-9e6c-5a8a60de8099",
+    "LI_SharessHarness_Nightsong_4_2053f2c9-a5b2-4fac-9739-49fc23ea6122"
+});
+_Nightsong:registerHandlers();
