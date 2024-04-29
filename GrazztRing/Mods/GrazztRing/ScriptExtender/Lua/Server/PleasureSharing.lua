74948d9e6a59fd805cd04321a9a95064f304e8e1
LazyIcarus <kleinmorett@gmail.com>
2024-04-29 17:33:31 -0400

    Limit pleasure sharing to 9m (30ft) range

--- GrazztRing/Mods/GrazztRing/ScriptExtender/Lua/Server/PleasureSharing.lua
+++ GrazztRing/Mods/GrazztRing/ScriptExtender/Lua/Server/PleasureSharing.lua
@@ -6,16 +6,20 @@ function SharePleasureModifier(character, causee, props)
     local is_source = Osi.HasActiveStatus(character, PLEASURE_SHARING_SOURCE) == 1;
     local is_target = Osi.HasActiveStatus(character, PLEASURE_SHARING_TARGET) == 1;
     if is_source or is_target then
-        local reduction = math.ceil(props.pleasure * 0.5);
-        props.pleasure = props.pleasure - reduction;
-        _P("Sharing pleasure " .. character .. " is_source " .. tostring(is_source) .. " is_target " .. tostring(is_target) .. " reduction " .. reduction .. " new pleasure " .. props.pleasure);
-        -- find the target to share with
-        if is_source then
-            local target = SHARING_CHARS[PLEASURE_SHARING_TARGET];
-            Osi.ApplyStatus(target, Mods.DivineCurse.PLEASURE_STATUS, 6 * reduction, 1, character);
-        else
-            local source = SHARING_CHARS[PLEASURE_SHARING_SOURCE];
-            Osi.ApplyStatus(source, Mods.DivineCurse.PLEASURE_STATUS, 6 * reduction, 1, character);
+        local target = SHARING_CHARS[PLEASURE_SHARING_TARGET];
+        local source = SHARING_CHARS[PLEASURE_SHARING_SOURCE];
+        -- check distance
+        local distance = Osi.GetDistanceTo(target, source);
+        if distance <= 9.0 then
+            local reduction = math.ceil(props.pleasure * 0.5);
+            props.pleasure = props.pleasure - reduction;
+            _P("Sharing pleasure " .. character .. " is_source " .. tostring(is_source) .. " is_target " .. tostring(is_target) .. " reduction " .. reduction .. " new pleasure " .. props.pleasure .. " distance " .. distance);
+            -- find the target to share with
+            if is_source then
+                Osi.ApplyStatus(target, Mods.DivineCurse.PLEASURE_STATUS, 6 * reduction, 1, character);
+            else
+                Osi.ApplyStatus(source, Mods.DivineCurse.PLEASURE_STATUS, 6 * reduction, 1, character);
+            end
         end
     end
 end
