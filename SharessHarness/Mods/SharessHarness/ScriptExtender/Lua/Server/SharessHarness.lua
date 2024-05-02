329bd05e04d4e90693205e8fdc3d1a1f1ebc4669
LazyIcarus <kleinmorett@gmail.com>
2024-05-01 23:37:52 -0400

    Auto adjust sharess embrace to body instead of requiring level up

--- SharessHarness/Mods/SharessHarness/ScriptExtender/Lua/Server/SharessHarness.lua
+++ SharessHarness/Mods/SharessHarness/ScriptExtender/Lua/Server/SharessHarness.lua
@@ -1,7 +1,3 @@
-local remodelled_frame_id_prefix = "LI_Claws_RemodelledFrame_";
-local stim_surge_id = "Shout_LI_Stim_Surge";
-local stim_status_prefix = "LI_STIM_";
-local stim_surge_level_increment = 3;
 local stimulation_passive = "LI_Sharess_Stimulation_Passive";
 
 local sated_status_ids = {
@@ -9,150 +5,31 @@ local sated_status_ids = {
     "LI_SATED_1",
     "LI_SATED_2"
 };
-local need_more_sated_stacks_msg = "LI_NEED_MORE_SATED_MSG";
-
-local function _I(msg)
-    _P("[SharessHarness] " .. msg);
-end
 
 
-function RemodelledFrameLevel(char)
-    for level = 1, 4 do
-        local passive_key = remodelled_frame_id_prefix .. level;
-        if Osi.HasPassive(char, passive_key) == 1 then
-            return level;
-        end
-    end
-    return 0;
-end
-
----@class LiHarnessProgression
+---@class LiHarnessProgression : BodyEquipment
 LiHarnessProgression = {};
----Main progression interface for Sharess's harness
+setmetatable(LiHarnessProgression, {
+    __index = Mods.DivineCurse.BodyEquipment
+});
+
+
+-- create the new function (constructor) with different arguments
 ---@param main_shortname string Short name for logging
----@param item_ids table list of full template ID for the armor item e.g. LI_LoviatarClaws_07ea67e5-344b-4de6-91d3-449cce27a685
+---@param item_ids_per_stage table VisualBank IDs for each item per body stage
 ---@return LiHarnessProgression
-function LiHarnessProgression:new(main_shortname, item_ids)
-    local self = {} ---@class LiHarnessProgression
+function LiHarnessProgression:new(main_shortname, item_ids_per_stage)
+    ---@class LiHarnessProgression
+    local self = Mods.DivineCurse.BodyEquipment:new(main_shortname, "Breast", item_ids_per_stage);
     setmetatable(self, {
         __index = LiHarnessProgression
-    })
-
-    self.shortname = main_shortname;
-    self.item_ids = item_ids;
+    });
 
     return self;
 end
 
-function LiHarnessProgression:log(message)
-    _P("[" .. self.shortname .. "] " .. message);
-end
-
-function LiHarnessProgression:harnessLevel(char)
-    local worn = Osi.GetEquippedItem(char, "Breast");
-    -- self:log("Character " .. char .. " wearing breasts " .. tostring(worn));
-    if worn == nil then
-        return nil;
-    end
-    for level = 1, 5 do
-        local harness_template_id = self.item_ids[level];
-        if Osi.GetTemplate(worn) == harness_template_id then
-            return level - 1;
-        end
-    end
-    return nil;
-end
-
----return a table of player character IDs that are wearing some copy of the item
----@return table player_id to number of sated stacks
-function LiHarnessProgression:getSatedWearers()
-    local sated = {};
-    -- return the character and the number of sated stacks
-    for _, player in pairs(Osi["DB_Players"]:Get(nil)) do
-        local char = player[1];
-        local found = false;
-        for stacks, status_id in pairs(sated_status_ids) do
-            if Osi.HasActiveStatus(char, status_id) == 1 then
-                self:log("Found sated wearer: " .. char .. " with stacks " .. stacks);
-                sated[char] = stacks;
-                found = true;
-            end
-        end
-        if found == false then
-            -- check if this character is wearing it but has no stacks
-            local harness_level = self:harnessLevel(char);
-            if harness_level ~= nil then
-                sated[char] = 0;
-            end
-        end
-    end
-    return sated;
-end
-
-local function delayedCall(delayInMs, func)
-    local startTime = Ext.Utils.MonotonicTime()
-    local handlerId;
-    handlerId = Ext.Events.Tick:Subscribe(function()
-        local endTime = Ext.Utils.MonotonicTime()
-        if (endTime - startTime > delayInMs) then
-            Ext.Events.Tick:Unsubscribe(handlerId)
-            func()
-        end
-    end);
-end
-
-function LiHarnessProgression:upgradeArmor(char)
-    -- check level of remodelled body
-    local body_level = RemodelledFrameLevel(char);
-    -- can only upgrade to the remodelled body stage
-    local armor_level = self:harnessLevel(char);
-    -- not wearing the harness
-    -- TODO give a message to the player that they need to be wearing the harness
-    if armor_level == nil then
-        self:log("Character " .. char .. " is not wearing the harness");
-        -- Osi.OpenMessageBox(char, acquire_message);
-        return
-    end
-
-    self:log("Body level " .. body_level .. " armor level " .. tostring(armor_level));
-    if body_level > armor_level then
-        -- remove the old armor
-        local old_armor_template = self.item_ids[armor_level + 1];
-        Osi.TemplateRemoveFromUser(old_armor_template, char, 1);
-
-        -- add the new armor
-        local new_armor_template = self.item_ids[armor_level + 2];
-        local new_armor = Osi.CreateAtObject(new_armor_template, char, 0, 0, "", 0);
-        self:log("Equipping new armor " .. tostring(new_armor));
-        Osi.Equip(char, new_armor);
-    end
-end
-
-function LiHarnessProgression:curseLongRestHandler()
-    self:log("long rest handler start")
-    -- loop over characters and check if anyone has 3 sated stacks
-    local wearers = self:getSatedWearers();
-    for wearer, stacks in pairs(wearers) do
-        -- check that the armor can still be upgraded
-        local armor_level = self:harnessLevel(wearer);
-        self:log("Wearer found: " .. wearer .. " with " .. stacks .. " stacks with armor level " .. tostring(armor_level));
-        -- armor level is from 0 to 4, so if it's 4 then we can't upgrade it anymore
-        if armor_level ~= nil and armor_level < 4 then
-            -- if we have fewer than 3 stacks, we don't need to do anything
-            -- give a message to the player hinting that they need to get more stacks
-            if stacks >= 2 then
-                self:upgradeArmor(wearer);
-            elseif stacks >= 0 then
-                self:log("Insufficient stacks: " .. stacks .. " for wearer " .. wearer .. ", giving message")
-                Osi.OpenMessageBox(wearer, need_more_sated_stacks_msg);
-            end
-        end
-    end
-end
-
-local clear_sated_registered = false;
-function LiHarnessProgression:clearSatedHandler()
-    delayedCall(3000, function()
+function ClearSatedHandler()
+    Mods.DivineCurse.DelayedCall(3000, function()
         for _, player in pairs(Osi["DB_Players"]:Get(nil)) do
             local char = player[1];
             -- remove sated status (doing it manually so it doesn't get cleared automatically after long rest)
@@ -161,37 +38,6 @@ function LiHarnessProgression:clearSatedHandler()
             end
         end
     end);
-end
-
-function LiHarnessProgression:registerHandlers()
-    Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", function() self:curseLongRestHandler() end);
-    if not clear_sated_registered then
-        Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", function() self:clearSatedHandler() end);
-        clear_sated_registered = true;
-    end
-    self:log("Registered handlers");
-end
-
-function GetStimLevel(char)
-    for level = 0, 9 do
-        local stim_id = stim_status_prefix .. level;
-        if Osi.HasActiveStatus(char, stim_id) == 1 then
-            return level;
-        end
-    end
-    return nil;
-end
-
-function HandleStimSurge(caster, spell, spellType, spellElement, storyActionID)
-    -- _I("Spell casted: " .. spell .. " by " .. caster .. " type " .. spellType .. " element " .. spellElement .. " story action " .. storyActionID);
-    -- if spell == stim_surge_id then
-    --     -- check what level of stim they have
-    --     local current_stim_level = GetStimLevel(caster) or 0;
-    --     local new_stim_level = math.min(current_stim_level + stim_surge_level_increment, 9);
-    --     Osi.RemoveStatus(caster, stim_status_prefix .. current_stim_level);
-    --     Osi.ApplyStatus(caster, stim_status_prefix .. new_stim_level, 1, 1, caster);
-    --     _I("Stim surge: " .. caster .. " stim level " .. current_stim_level .. " -> " .. new_stim_level);
-    -- end
 end
 
 function HandleSatedFromBliss(char, status, causee, storyActionID)
@@ -222,7 +68,7 @@ function HandleSatedFromBliss(char, status, causee, storyActionID)
     end
 end
 
-Ext.Osiris.RegisterListener("CastedSpell", 5, "after", HandleStimSurge);
+Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", function() ClearSatedHandler() end);
 Ext.Osiris.RegisterListener("StatusApplied", 4, "after", HandleSatedFromBliss);
 
 Harness = LiHarnessProgression:new("Harness", {
