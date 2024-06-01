f83ce7daace957d507ac0b51253b2459c8aa5242
LazyIcarus <kleinmorett@gmail.com>
2024-05-31 20:59:17 -0400

    Add appearance debugger back in

--- DivineCurse/Mods/DivineCurse/ScriptExtender/Lua/Server/AppearanceDebugger.lua
+++ DivineCurse/Mods/DivineCurse/ScriptExtender/Lua/Server/AppearanceDebugger.lua
@@ -1,14 +1,14 @@
 -- get body override debug item
 PersistentVars = {};
--- local debug_item_id = "LI_Body_Debugger_5c4ace59-3a59-4dc0-b6dc-b6049b119a02";
--- local function addDebugItem()
---     if PersistentVars[debug_item_id] == nil then
---         local char = Osi.GetHostCharacter();
---         _P("Added body debug item to " .. tostring(char));
---         Osi.TemplateAddTo(debug_item_id, char, 1, 1)
---         PersistentVars[debug_item_id] = true;
---     end
--- end
+local debug_item_id = "LI_Body_Debugger_5c4ace59-3a59-4dc0-b6dc-b6049b119a02";
+local function addDebugItem()
+    if PersistentVars[debug_item_id] == nil then
+        local char = Osi.GetHostCharacter();
+        _P("Added body debug item to " .. tostring(char));
+        Osi.TemplateAddTo(debug_item_id, char, 1, 1)
+        PersistentVars[debug_item_id] = true;
+    end
+end
 
--- Ext.Osiris.RegisterListener("CharacterCreationFinished", 0, "after", function() addDebugItem() end);
--- Ext.Osiris.RegisterListener("SavegameLoaded", 0, "after", function() addDebugItem() end);
+Ext.Osiris.RegisterListener("CharacterCreationFinished", 0, "after", function() addDebugItem() end);
+Ext.Osiris.RegisterListener("SavegameLoaded", 0, "after", function() addDebugItem() end);
